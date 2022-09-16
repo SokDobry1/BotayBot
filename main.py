c17b2497874df0cfe6f34aa696daaf6b55383b51
SokDobry1 <officialaccalf@gmail.com>
2022-09-16 16:18:01 +0300

    Попытка исправить отключение от вк

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ if __name__ == '__main__':
     th_timer = Thread(target=timer)
     th_timer.start()
     while True:
-        for event in longpoll.listen():
-            #Если пришло новое сообщение
-            if event.type == VkEventType.MESSAGE_NEW:
-                if event.to_me:
-                    request = {"body": event.text,
-                            "user_id": event.user_id}
-                    try:
-                        request.update({"payload": event.payload})
-                    except:
-                        pass
-                    
-                    message_handler.create_answer(request)
-
+        try:
+            for event in longpoll.listen():
+                #Если пришло новое сообщение
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.to_me:
+                        request = {"body": event.text,
+                                "user_id": event.user_id}
+                        try:
+                            request.update({"payload": event.payload})
+                        except:
+                            pass
+                        
+                        message_handler.create_answer(request)
+        except: from vkapi import longpoll
 
 
 
